5ab9ce3e774da73ae8b9e31b8a90adc4bfff4e32
Mustajab <mustajabsa@gmail.com>
2022-06-08 18:22:02 +0700

    Add Eureka Client Annotation

--- transaksi/src/main/java/com/lawencon/transaction/TransaksiApplication.java
+++ transaksi/src/main/java/com/lawencon/transaction/TransaksiApplication.java
@@ -2,8 +2,10 @@ package com.lawencon.transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class TransaksiApplication {
 
 	public static void main(String[] args) {
